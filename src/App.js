b85d8a3ca5ec71d2e92132f8c373b840e47f0768
i-Lene <marlene.s.veiga@gmail.com>
2023-05-30 23:04:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import QuoteTemplate from './components/QuoteTemplate';
+
+
+
 
 function App() {
+
+  const [quote, setQuote] = useState({
+    content: 'Every time you smile at someone, it is an action of love, a gift to that person, a beautiful thing.',
+    author: 'Mother Teresa'
+  });
+  const [allQuotes, setAlllQuotes] = useState([]);
+
+  useEffect(() => {
+    fetch('https://type.fit/api/quotes')
+      .then(res => res.json())
+      .then(data => setAlllQuotes(data))
+  }, [])
+
+
+  function handleOnCLick() {
+    const random = Math.floor(Math.random() * allQuotes.length);
+    const quote_value = allQuotes[random];
+
+    setQuote({
+      content: quote_value.text,
+      author: quote_value.author
+    })
+
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = Math.floor(Math.random() * 30) + 70;
+    const lightness = Math.floor(Math.random() * 20) + 70;
+
+    const randomColor = "hsl(" + hue + ", " + saturation + "%, " + lightness + "%)";
+    document.documentElement.style.setProperty('--main-bg-color', randomColor);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteTemplate
+        newQuote={handleOnCLick}
+        content={quote.content}
+        author={quote.author}
+      />
     </div>
   );
 }
---
+++ src/components/QuoteTemplate.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+export default function QuoteTemplate(props) {
+    return (
+        <div id="quote-box">
+            <div className="quote-text">
+                <span id="text">{props.content}</span>
+            </div>
+            <div className="quote-author">
+                <span id="author"> - {props.author}</span>
+            </div>
+            <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+            <button id="new-quote" onClick={props.newQuote} >New Quote</button>
+        </div>
+    )
+}
