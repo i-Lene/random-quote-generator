6cbb5c12600e4dcd19d4db6efcb25304e21610c9
i-Lene <marlene.s.veiga@gmail.com>
2023-05-30 23:36:13 +0100

    twitter

--- src/components/QuoteTemplate.jsx
+++ src/components/QuoteTemplate.jsx
@@ -3,6 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export default function QuoteTemplate(props) {
+
+    const tweetText = encodeURIComponent(props.content);
+
     return (
         <div id="quote-box">
             <div className="quote-text">
@@ -11,7 +14,7 @@ export default function QuoteTemplate(props) {
             <div className="quote-author">
                 <span id="author"> - {props.author}</span>
             </div>
-            <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${tweetText}`} target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
             <button id="new-quote" onClick={props.newQuote} >New Quote</button>
         </div>
     )
